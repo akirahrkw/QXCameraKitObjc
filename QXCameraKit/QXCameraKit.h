f4892709ca123dea37763ceaa4ba4585fd11982f
akirahrkw <akirahrkw@gmail.com>
2014-10-05 16:09:32 +0800

    Initial Commit

---
+++ QXCameraKit/QXCameraKit.h
@@ -0,0 +1,19 @@
+//
+//  QXCameraKit.h
+//  QXCameraKit
+//
+//  Created by Hirakawa Akira on 5/10/14.
+//  Copyright (c) 2014 Hirakawa Akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QXCameraKit.
+FOUNDATION_EXPORT double QXCameraKitVersionNumber;
+
+//! Project version string for QXCameraKit.
+FOUNDATION_EXPORT const unsigned char QXCameraKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QXCameraKit/PublicHeader.h>
+
+
